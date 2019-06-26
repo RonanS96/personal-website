c0173ef3c262b67edb5b1e81f7d43996ea76f528
Ronan Smith <Ronan Smith>
2019-06-26 21:30:43 +0100

    Loader for images and highlight active page on navbar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,44 @@
 import React from "react";
+import Img from "react-image";
 
-function HomePage() {
-  return (
-    <div>
-        <header className="w3-container w3-center home-header" id="home">
-            <h1 className="w3-jumbo main-title"><span className="w3-hide-small">I'm</span> Ronan Smith.</h1>
-            <p className="main-subtitle"><span className="w3-hide-small">Computer Scientist & </span>Software Developer.</p>
-            <img className="main-photo" src="me-cropped.jpg" alt="me" />
-        </header>
-    </div>
-  );
+export class HomePage extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      isLoading: true
+    }
+
+    this.hideLoader = this.hideLoader.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+          <header className="w3-container w3-center home-header" id="home">
+              <h1 className="w3-jumbo main-title"><span className="w3-hide-small">I'm</span> Ronan Smith.</h1>
+              <p className="main-subtitle"><span className="w3-hide-small">Computer Scientist & </span>Software Developer.</p>
+              <Img
+                className="main-photo"
+                src="me-cropped.jpg"
+                alt="me"
+                loader={this.loader()}
+              />
+          </header>
+      </div>
+    );
+  }
+
+  loader() {
+    return (
+      <img className="main-photo spinner" src="images/loading-spinner.gif"></img>
+    )
+  }
+
+  hideLoader() {
+    this.setState({
+      isLoading: false
+    })
+  }
 }
 
 export default HomePage;
